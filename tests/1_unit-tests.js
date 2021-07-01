3dbc1a2b75e8a44799c3e42e4d9941c15e67d326
Willyanto <willyanto39@gmail.com>
2021-07-01 21:25:07 +0700

    Add test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,122 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const messages = require('../constants/messages');
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings');
+
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('UnitTests', () => {
+  suite('Test solver.validate()', () => {
+    test('Puzzle with 81 Characters', (done) => {
+      const validity = solver.validate(puzzlesAndSolutions[0][0]);
 
+      assert.isTrue(validity.valid);
+      done();
+    });
+    
+    test('Puzzle with Invalid Characters', (done) => {
+      const puzzleStringArr = puzzlesAndSolutions[0][0].split('');
+      puzzleStringArr[0] = 'x';
+      const puzzleString = puzzleStringArr.join('');
+      const validity = solver.validate(puzzleString);
+
+      assert.isFalse(validity.valid);
+      assert.equal(validity.reason, messages.INVALID_CHARACTER);
+      done();
+    });
+
+    test('Puzzle That is not 81 Characters Long', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0] + '3';
+      const validity = solver.validate(puzzleString);
+
+      assert.isFalse(validity.valid);
+      assert.equal(validity.reason, messages.INCORRECT_LENGTH);
+      done();
+    });
+  });
+
+  suite('Test solver.checkRowPlacement()', () => {
+    test('Valid Row Placement', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const rowPlacement = solver.checkRowPlacement(puzzleString, 'A', '4', 7);
+
+      assert.isTrue(rowPlacement);
+      done();
+    });
+
+    test('Invalid Row Placement', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const rowPlacement = solver.checkRowPlacement(puzzleString, 'A', '2', 1);
+
+      assert.isFalse(rowPlacement);
+      done();
+    });
+  });
+
+  suite('Test solver.checkColumnPlacement()', () => {
+    test('Valid Column Placement', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const columPlacement = solver.checkColPlacement(puzzleString, 'C', '1', 7);
+
+      assert.isTrue(columPlacement);
+      done();
+    });
+
+    test('Invalid Column Placement', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const columPlacement = solver.checkColPlacement(puzzleString, 'B', '1', 1);
+
+      assert.isFalse(columPlacement);
+      done();
+    });
+  });
+
+  suite('Test solver.checkRegionPlacement()', () => {
+    test('Valid Region Placement', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const regionPlacement = solver.checkRegionPlacement(puzzleString, 'B', '1', 9);
+
+      assert.isTrue(regionPlacement);
+      done();
+    });
+
+    test('Invalid Region Placement', (done) => {
+      const puzzlestring = puzzlesAndSolutions[0][0];
+      const regionPlacement = solver.checkRegionPlacement(puzzlestring, 'B', '2', 5);
+
+      assert.isFalse(regionPlacement);
+      done();
+    });
+  });
+
+  suite('Test solver.solve()', () => {
+    test('Valid Puzzle Pass the Solver', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const solution = solver.solve(puzzleString);
+
+      assert.isOk(solution);
+      done();
+    });
+
+    test('Invalid Puzzle Fail the Solver', (done) => {
+      const puzzleStringArr = puzzlesAndSolutions[0][0].split('');
+      puzzleStringArr[1] = '1';
+      const puzzleString = puzzleStringArr.join('');
+
+      const solution = solver.solve(puzzleString);
+      
+      assert.isNotOk(solution);
+      done();
+    });
+
+    test('Solver Returns Expected Solution', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0];
+      const solution = solver.solve(puzzleString);
+
+      assert.equal(solution, puzzlesAndSolutions[0][1]);
+      done();
+    });
+  });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -3,9 +3,230 @@ const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
 
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings');
+const messages = require('../constants/messages');
+
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
+  suite('Test POST /api/solve', () => {
+    test('Valid Puzzle String', (done) => {
+      chai
+        .request(server)
+        .post('/api/solve')
+        .send({ puzzle: puzzlesAndSolutions[0][0] })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.solution, puzzlesAndSolutions[0][1]);
+          done();
+        });
+    });
 
+    test('Missing Puzzle String', (done) => {
+      chai
+        .request(server)
+        .post('/api/solve')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.MISSING_FIELD);
+          done();
+        });
+    });
+
+    test('Invalid Characters', (done) => {
+      const puzzleStringArr = puzzlesAndSolutions[0][0].split('');
+      puzzleStringArr[0] = 'x';
+      const puzzleString = puzzleStringArr.join('');
+
+      chai
+        .request(server)
+        .post('/api/solve')
+        .send({ puzzle: puzzleString })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.INVALID_CHARACTER);
+          done();
+        });
+    });
+
+    test('Invalid Length', (done) => {
+      chai
+        .request(server)
+        .post('/api/solve')
+        .send({ puzzle: 'a' })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.INCORRECT_LENGTH);
+          done();
+        });
+    });
+
+    test('Unsolvable Puzzle', (done) => {
+      const puzzleStringArr = puzzlesAndSolutions[0][0].split('');
+      puzzleStringArr[1] = '1';
+      const puzzleString = puzzleStringArr.join('');
+
+      chai
+        .request(server)
+        .post('/api/solve')
+        .send({ puzzle: puzzleString })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.NO_SOLUTION);
+          done();
+        });
+    });
+  });
+
+  suite('Test POST /api/check', () => {
+    test('All Fields', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+          coordinate: 'C1',
+          value: '7'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isTrue(res.body.valid);
+          done();
+        });
+    });
+
+    test('Single Placement Conflict', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+          coordinate: 'C9',
+          value: '1'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.conflict.length, 1);
+          done();
+        });
+    });
+
+    test('Multiple Placement Conflict', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+          coordinate: 'B2',
+          value: '7'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.conflict.length, 2);
+          done();
+        });
+    });
+
+    test('All Placement Conflict', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+          coordinate: 'B5',
+          value: '2'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.conflict.length, 3);
+          done();
+        });
+    });
+
+    test('Missing Required Fields', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          coordinate: 'B7',
+          value: '5'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.MISSING_FIELDS);
+          done();
+        });
+    });
+
+    test('Invalid Character', (done) => {
+      const puzzleStringArr = puzzlesAndSolutions[0][0].split('');
+      puzzleStringArr[0] = 'x';
+      const puzzleString = puzzleStringArr.join('');
+
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzleString,
+          coordinate: 'A4',
+          value: '7'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.INVALID_CHARACTER);
+          done();
+        });
+    });
+
+    test('Incorrect Length', (done) => {
+      const puzzleString = puzzlesAndSolutions[0][0] + '8';
+
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzleString,
+          coordinate: 'A1',
+          value: '5'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.INCORRECT_LENGTH);
+          done();
+        });
+    });
+
+    test('Invalid Coordinate', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+          coordinate: 'Z5',
+          value: '6'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.INVALID_COORDINATE);
+          done();
+        });
+    });
+
+    test('Invalid Value', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+          coordinate: 'F5',
+          value: 'a'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, messages.INVALID_VALUE);
+          done();
+        });
+    });
+  });
 });
 
